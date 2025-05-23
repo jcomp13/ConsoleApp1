b08718704a6d9e5abcfbf4570e025f178bbf1fcf
Jeff Compell <jcomp13@yahoo.com>
2025-05-22 22:55:54 -0400

    Added math class with the add function

---
+++ ConsoleApp1/math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class math
+    {
+        internal int add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+            return sum;
+        }
+    }
+}
